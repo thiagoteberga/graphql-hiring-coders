eacac3211682a23c839273a20643c0e78084d2c0
thiagoteberga <thiagoteberga@live.com>
2021-08-06 23:54:44 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Curso de GraphQl
+> Projeto desenvolvido durante o Hiring Coders (Em parceria com a Gama Academy)
+
+## Andamento do Curso
+- [x] - 01 - Apresentação e Introdução
+- [x] - 02 - Modelo Cliente e Servidor
+- [x] - 03 - Padrão de Requisição e Resposta
+- [x] - 04 - Iniciando o projeto de exemplo
+- [x] - 05 - Criando o servidor
+- [x] - 06 - Renderização, parâmetros e redirecionamentos
+- [ ] - 07 - Monorepo
+- [ ] - 08 - Pacote com React.js
+- [ ] - 09 - Rotas com react-router-dom
+- [ ] - 10 - Mudando server para express
+- [ ] - 11 - Requisição assíncrona
+- [ ] - 12 - Resolvendo CORS
+- [ ] - 13 - APIs RESTful
+- [ ] - 14 - Apollo Server
+- [ ] - 15 - TypeDefs e resolvers
+- [ ] - 16 - Introdução a GraphQL Query
+- [ ] - 17 - Criando GraphQL Queries
+- [ ] - 18 - Paginação, ordenação e filtros
+- [ ] - 19 - GraphQL Mutation
+- [ ] - 20 - Mutation para alterar e excluir clients
+- [ ] - 21 - Configurando o Apollo Link
+- [ ] - 22 - Criando o Apollo Link, o Logger
+- [ ] - 23 - Apollo Client e Apollo DevTools
+- [ ] - 24 - Hook useQuery e paginação
+- [ ] - 25 - Hook useMutation
+
+
+# Links Úteis:
+## Introduction to GraphQL:
+https://graphql.org/learn/
+
+
+# Comando para rodar em uma porta especifica:
+
+``` bash
+# Port is a variable
+set PORT=3000 && npm run start
+
+```
---
+++ src/main.js
@@ -0,0 +1,77 @@
+import { createServer } from 'http';
+import { readFile } from 'fs';
+import { resolve } from 'path';
+import { parse } from 'querystring';
+
+const server = createServer((request,response)=>{
+    switch(request.url){
+        case '/status': {
+            response.writeHead(200, {'Content-Type': 'application/json'})
+            response.write(JSON.stringify({
+                status: 'Okay'
+            }))
+            response.end();
+            break;
+        }
+        case '/sign-in': {
+            const path = resolve(__dirname,'./pages/sign-in.html');
+            readFile(path,(error,file) => {
+                if(error){
+                    response.writeHead(500, {'Content-Type': 'application/json'})
+                    response.write(JSON.stringify({
+                        status: 'Can\'t process HTML file.'
+                    }))
+                    response.end();
+                    return;
+                }
+
+                response.writeHead(200)
+                response.write(file);
+                response.end();
+            })
+            break;
+        }
+        case '/authenticate': {
+            let data = '';
+            request.on('data', (chunk) => data += chunk);
+            request.on('end', ()=> {
+                const params = parse(data);
+                //console.log(parse(data));
+                response.writeHead(301,{
+                    Location: '/home',
+                });
+                response.end();
+            });
+            break;
+        }
+        case '/home': {
+            const path = resolve(__dirname,'./pages/home.html');
+            readFile(path,(error,file) => {
+                if(error){
+                    response.writeHead(500, {'Content-Type': 'application/json'})
+                    response.write(JSON.stringify({
+                        status: 'Can\'t process HTML file.'
+                    }))
+                    response.end();
+                    return;
+                }
+
+                response.writeHead(200)
+                response.write(file);
+                response.end();
+            })
+            break;
+        }
+        default: {
+            response.writeHead(404, 'Service not found on server');
+            response.end();
+        }
+    }
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1'
+
+server.listen(PORT,HOSTNAME,() => {
+    console.log(`Server is running at http://${HOSTNAME}:${PORT}`);
+})
